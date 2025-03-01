2168244559dbb99dfb873b27b0fc22f967277aa8
Daniel Carson <DCarson@apus.edu>
2025-03-01 13:58:34 -0500

    Added xUnit Test project. Added tests for GetPeople, GetPersonByName

---
+++ Stargate.Server.Tests/GetPeopleTests.cs
@@ -0,0 +1,128 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Stargate.Server.Business.Queries;
+using Stargate.Server.Data.Models;
+using Stargate.Server.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stargate.Server.Tests;
+
+public class GetPeopleTests
+{
+    [Fact]
+    public async Task GetPeople_ShouldCall_GetPersonAstronautAllAsync_Once()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPeople() { };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        mockedPersonRepo.Verify(m => m.GetPersonAstronautAllAsync(default), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetPeople_ShouldReturnOKStatusCode_WhenRequestIsValid()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        IEnumerable<PersonAstronaut> people = new List<PersonAstronaut>() { };
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();
+        mockedPersonRepo.Setup(x => x.GetPersonAstronautAllAsync(default)).Returns(Task.FromResult(people));
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPeople() { };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Success.Should().BeTrue();
+        response.ResponseCode.Should().Be((int)HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetPeople_ShouldBeValidGetPeopleResult_WhenRequestIsValid()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        var person1 = new PersonAstronaut
+        {
+            PersonId = 1,
+            Name = "Dan Carson"
+        };
+
+        var person2 = new PersonAstronaut
+        {
+            PersonId = 2,
+            Name = "John Smith",
+            CurrentRank = "2LT",
+            CurrentDutyTitle = "Commander",
+            CareerStartDate = new DateTime(2025, 3, 1)
+        };
+
+        IEnumerable<PersonAstronaut> people = new List<PersonAstronaut>() { person1, person2 };
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();
+        mockedPersonRepo.Setup(x => x.GetPersonAstronautAllAsync(default)).Returns(Task.FromResult(people));
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPeople() { };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.ResponseCode.Should().Be((int)HttpStatusCode.OK);
+        response.Success.Should().BeTrue();
+        response.People.Count.Should().Be(people.Count());
+        CheckEqual(response.People[0], person1).Should().BeTrue();
+        CheckEqual(response.People[1], person2).Should().BeTrue();
+    }
+
+    private bool CheckEqual(PersonAstronaut a, PersonAstronaut b)
+    {
+        return a.PersonId == b.PersonId
+            && a.Name.Equals(b.Name)
+            && (a.CurrentRank ?? "").Equals(b.CurrentRank ?? "")
+            && (a.CurrentDutyTitle ?? "").Equals(b.CurrentDutyTitle ?? "")
+            && a.CareerStartDate.Equals(b.CareerStartDate)
+            && a.CareerEndDate.Equals(b.CareerEndDate);
+    }
+}
---
+++ Stargate.Server.Tests/GetPersonByNameTests.cs
@@ -0,0 +1,152 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Stargate.Server.Business.Queries;
+using Stargate.Server.Data.Models;
+using Stargate.Server.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stargate.Server.Tests;
+
+public class GetPersonByNameTests
+{
+    [Fact]
+    public async Task GetPersonByName_ShouldCall_GetPersonAstronautByNameAsync_Once()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();        
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPersonByName() { Name = "Dan Carson" };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        mockedPersonRepo.Verify(m => m.GetPersonAstronautByNameAsync(It.IsAny<string>(), default), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetPersonByName_ShouldReturnNotFound_WhenPersonDoesNotExist()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();
+        mockedPersonRepo.Setup(x => x.GetPersonAstronautByNameAsync(It.IsAny<string>(), default)).Returns(Task.FromResult((PersonAstronaut?)null));
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPersonByName() { Name = "Dan Carson" };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        response.ResponseCode.Should().Be((int)HttpStatusCode.NotFound);
+        response.Success.Should().BeFalse();
+        response.Person.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task GetPersonByName_ShouldReturnOk_WhenPersonExists()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        const string expName = "Dan Carson";
+
+        var person = new PersonAstronaut
+        {
+            PersonId = 1,
+            Name = expName
+        };
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();
+        mockedPersonRepo.Setup(x => x.GetPersonAstronautByNameAsync(expName, default)).Returns(Task.FromResult((PersonAstronaut?)person));
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPersonByName() { Name = expName };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        response.ResponseCode.Should().Be((int)HttpStatusCode.OK);
+        response.Success.Should().BeTrue();
+        response.Person.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetPersonByName_ShouldBeValidGetPersonByNameResult_WhenPersonExists()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        
+        const int expPersonId = 1;
+        const string expName = "Dan Carson";
+        const string expRank = "2LT";
+        const string expTitle = "Commander";
+        DateTime expCareerStartDate = new DateTime(2025, 3, 1);
+        DateTime? expCareerEndDate = null;
+
+        var person = new PersonAstronaut
+        {
+            PersonId = expPersonId,
+            Name = expName,
+            CurrentRank = expRank,
+            CurrentDutyTitle = expTitle,
+            CareerStartDate = expCareerStartDate,
+            CareerEndDate = expCareerEndDate
+        };
+
+        var mockedPersonRepo = new Mock<IPersonRepository>();
+        mockedPersonRepo.Setup(x => x.GetPersonAstronautByNameAsync(expName, default)).Returns(Task.FromResult((PersonAstronaut?)person));
+
+        var serviceProvider = services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetPersonByNameHandler).Assembly))
+            .AddScoped<IPersonRepository>(x => mockedPersonRepo.Object)
+            .BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        var query = new GetPersonByName() { Name = expName };
+
+        // Act
+        var response = await mediator.Send(query);
+
+        // Assert
+        response.Person.Should().NotBeNull();
+        response.Person.PersonId.Should().Be(expPersonId);
+        response.Person.Name.Should().Be(expName);
+        response.Person.CurrentRank.Should().Be(expRank);
+        response.Person.CurrentDutyTitle.Should().Be(expTitle);
+        response.Person.CareerStartDate.Should().Be(expCareerStartDate);
+        response.Person.CareerEndDate.Should().Be(expCareerEndDate);
+    }
+}
