49c8c6096890cb730d201d1ab5242d4fd86e0cbe
Daniel Carson <DCarson@apus.edu>
2025-03-02 17:53:30 -0500

    Re-named css classes

--- stargate.client/src/app/astronaut/astronaut.component.ts
+++ stargate.client/src/app/astronaut/astronaut.component.ts
@@ -4,14 +4,14 @@ import { Astronaut } from '../astronaut.service';
 @Component({
   selector: 'app-astronaut',
   template: `
-    <section class="listing">
-      <img class="listing-photo" src="/assets/astronaut-9988.svg" alt="Photo of {{astronaut.name}}">
-      <h2 class="listing-heading">{{ astronaut.name }}</h2>
+    <section class="card">
+      <img class="card-photo" src="/assets/astronaut-9988.svg" alt="Photo of {{astronaut.name}}">
+      <h2 class="card-heading">{{ astronaut.name }}</h2>
       @if(astronaut.careerStartDate === null)
       {
-        <p class="listing-location">Civilian<br/>Potential Astronaut in Training!</p>
+        <p class="card-description">Civilian<br/>Potential Astronaut in Training!</p>
       } @else {        
-        <p class="listing-location">Astronuat<br/>{{astronaut.currentDutyTitle }}, {{ astronaut.currentRank}}</p>
+        <p class="card-description">Astronuat<br/>{{astronaut.currentDutyTitle }}, {{ astronaut.currentRank}}</p>
       }
       
       <a [routerLink]="['/astronaut-details', astronaut.name]">Learn More</a>
