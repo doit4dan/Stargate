772c688bce63c29d44955f646a84a05544e33baf
Daniel Carson <DCarson@apus.edu>
2025-03-02 19:38:04 -0500

    Added Civilian specific content on astronaut detials component

--- stargate.client/src/app/astronaut-details/astronaut-details.component.ts
+++ stargate.client/src/app/astronaut-details/astronaut-details.component.ts
@@ -10,43 +10,55 @@ import { CommonModule } from '@angular/common';
       <img class="astronaut-photo" src="/assets/astronaut-9988.svg"
         alt="Photo of {{astronaut?.name}}"/>
       <section class="description-section">
-        <h2 class="astronaut-heading">{{astronaut?.name}}</h2>        
-        @if(astronaut?.careerEndDate === null)
-        {
-          <p class="description-text">
-            {{astronaut?.name}} is classified as an Astronaut in the Stargate system. 
-            This person is currently serving in the {{astronaut?.currentDutyTitle}} role with
-            a rank of {{astronaut?.currentRank}}. They started their career as an astronaut
-            on {{astronaut?.careerStartDate | date:'MMMM, dd, yyyy'}}. Please see their full career history below.
-          </p>     
+        <h2 class="astronaut-heading">{{astronaut?.name}}</h2>  
+        @if(astronaut?.careerStartDate !== null)          
+        {      
+          @if(astronaut?.careerEndDate === null)
+          {
+            <p class="description-text">
+              {{astronaut?.name}} is classified as an Astronaut in the Stargate system. 
+              This person is currently serving in the {{astronaut?.currentDutyTitle}} role with
+              a rank of {{astronaut?.currentRank}}. They started their career as an astronaut
+              on {{astronaut?.careerStartDate | date:'MMMM, dd, yyyy'}}. Please see their full career history below.
+            </p>     
+          } @else {
+            <p class="description-text">
+              {{astronaut?.name}} is classified as an Retired Astronaut in the Stargate system. 
+              They started their career as an astronaut
+              on {{astronaut?.careerStartDate | date:'MMMM, dd, yyyy'}} and ended their career on {{astronaut?.careerEndDate | date:'MMMM, dd, yyyy'}}. 
+              They retired with a rank of {{astronaut?.currentRank}}. Please see their full career history below.
+            </p> 
+          }  
         } @else {
           <p class="description-text">
-            {{astronaut?.name}} is classified as an Retired Astronaut in the Stargate system. 
-            They started their career as an astronaut
-            on {{astronaut?.careerStartDate | date:'MMMM, dd, yyyy'}} and ended their career on {{astronaut?.careerEndDate | date:'MMMM, dd, yyyy'}}. 
-            They retired with a rank of {{astronaut?.currentRank}}. Please see their full career history below.
-          </p> 
-        }          
-      </section>            
-      <section>       
-       <h2 class="section-heading">Duty History</h2>     
-        <table class="duty-table">
-          <tr>
-            <th>Rank</th>
-            <th>Duty Title</th>
-            <th>Duty Start</th>
-            <th>Duty End</th>
-          </tr>
-          @for (item of astronautDuties; track item.id) {
+            {{astronaut?.name}} is classified as an Civilian in the Stargate system. 
+            This person is happily employed by Stargate and is responsible for managing records and maintenance of the system. 
+            They may also be enrolled in several free astronaut training courses provided by Stargate to potentially become an astronaut one day themselves. 
+          </p>     
+        }
+      </section>  
+      @if(astronaut?.careerStartDate !== null)          
+      {
+        <section>       
+          <h2 class="section-heading">Duty History</h2>     
+          <table class="duty-table">
             <tr>
-              <td>{{ item?.rank}}</td>
-              <td>{{item?.dutyTitle }}</td>
-              <td>{{item?.dutyStartDate | date:'MM.dd.yyyy'}}</td>
-              <td>{{item?.dutyEndDate | date:'MM.dd.yyyy'}}</td>
+              <th>Rank</th>
+              <th>Duty Title</th>
+              <th>Duty Start</th>
+              <th>Duty End</th>
             </tr>
-          }
-        </table>       
-      </section>           
+            @for (item of astronautDuties; track item.id) {
+              <tr>
+                <td>{{ item?.rank}}</td>
+                <td>{{item?.dutyTitle }}</td>
+                <td>{{item?.dutyStartDate | date:'MM.dd.yyyy'}}</td>
+                <td>{{item?.dutyEndDate | date:'MM.dd.yyyy'}}</td>
+              </tr>
+            }
+          </table>       
+      </section>
+    }                 
     </article>
   `,
   styleUrl: './astronaut-details.component.css'
